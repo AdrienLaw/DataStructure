cf167a9f57fc3afae71e75b79b1275f20992ecd4
adrien <adrienlaw@126.com>
2020-06-01 17:51:50 +0800

    双向链表局部

--- src/apple/linkedlist/SingleLinkedListDemo.java
+++ src/apple/linkedlist/SingleLinkedListDemo.java
@@ -14,8 +14,12 @@ public class SingleLinkedListDemo {
         HeroNode heroNode4 = new HeroNode(3, "吴用", "智多星");
         HeroNode heroNode5 = new HeroNode(5, "关胜", "大刀");
         HeroNode heroNode50 = new HeroNode(5, "关胜", "小刀");
+        HeroNode heroNode34 = new HeroNode(34, "解珍", "两头蛇");
+        HeroNode heroNode11 = new HeroNode(11, "李应", "扑天雕");
         singleLinkedList.addByOrder(heroNode4);
         singleLinkedList.addByOrder(heroNode50);
+        singleLinkedList.addByOrder(heroNode34);
+        singleLinkedList.addByOrder(heroNode11);
         singleLinkedList.list();
         System.out.println("=======================================");
         singleLinkedList.update(heroNode5);
@@ -23,7 +27,20 @@ public class SingleLinkedListDemo {
         System.out.println("=======================================");
         singleLinkedList.delete(5);
         singleLinkedList.list();
+
+        System.out.println("=======================================");
+        int length = singleLinkedList.getLength(heroNode3);
+        singleLinkedList.list();
+        System.out.println("============="+length);
+        System.out.println("=======================================");
+        HeroNode heroNode = singleLinkedList.lastIndexNode(heroNode1, 4);
+        System.out.println(heroNode);
+        System.out.println("=======================================");
+        singleLinkedList.reverseLinkedList(heroNode);
+        singleLinkedList.list();
     }
+
+
 }
 
 class SingleLinkedList {
@@ -169,8 +186,56 @@ class SingleLinkedList {
         }
     }
 
+    public int getLength(HeroNode heroNode) {
+        if (headNode.next == null) {
+            return 0;
+        }
+        int length = 0;
+        HeroNode curr = headNode.next;
+        while (curr != null) {
+            length ++;
+            curr = curr.next;
+        }
+        return length;
+    }
 
+    /**
+     * 查找倒数第 K 个节点
+     */
+    public HeroNode lastIndexNode(HeroNode headNode,int index) {
+        //如果链表是空心
+        if (headNode.next == null) {
+            return null;
+        }
+        //第一遍得到链表的长度
+        int size = getLength(headNode);
+        if (index <= 0 || index > size) {
+            return null;
+        }
+        HeroNode curr = headNode.next;
+        for (int i = 1;i < size - index; i++) {
+            curr = curr.next;
+        }
+        return curr;
+    }
 
+    //反转链表
+    public void reverseLinkedList(HeroNode headNode) {
+        if (headNode.next == null || headNode.next.next == null) {
+            //一个或空
+            return;
+        }
+        HeroNode curr = headNode.next;
+        HeroNode next = null;
+        HeroNode reverseHead = new HeroNode(0, "", "");
+        while (curr != null) {
+            next = curr.next;
+            curr.next = reverseHead.next;
+            reverseHead.next = curr;
+            curr = next;
+        }
+        headNode.next = reverseHead.next;
+    }
 }
 
 class HeroNode {
@@ -194,6 +259,5 @@ class HeroNode {
                 ", nickName='" + nickName + '\'' +
                 '}';
     }
-
-
 }
+
